32926470d46e73f16d07a023bfb828a90445c2de
Ali Hassnain <tohassnain@gmail.com>
2025-08-04 10:34:19 +0500

    Remove secrets

--- src/Firebase-admin.ts
+++ src/Firebase-admin.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApps, getApp, cert, App } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import serviceKey from "./service-key.json";
-import firebase from "firebase/compat/app";
 
 let app: App;
 if (getApps().length === 0) {
